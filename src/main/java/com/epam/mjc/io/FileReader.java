5c86996138f7c16daafee6ac4b6c8b0f084a945c
PHAS71 <129070669+PHAS71@users.noreply.github.com>
2023-07-15 17:58:45 +0300

    Add files via upload

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,29 @@
 package com.epam.mjc.io;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        try(FileInputStream fileInputStream = new FileInputStream(file)) {
+            int ch;
+            StringBuilder sb = new StringBuilder();
+            while((ch = fileInputStream.read()) != -1){
+                sb.append((char)ch);
+            }
+            String[] data = sb.toString().split(": | |\n |\r\n |\r");
+
+            String name = data[1];
+            int age = Integer.parseInt(data[3]);
+            String email = data[5];
+            Long phone = Long.parseLong(data[7]);
+
+            return new Profile(name, age, email, phone);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
